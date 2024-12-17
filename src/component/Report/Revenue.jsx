956531981989b752415d6041a05049c5ffb8ad04
lightingbro46 <quanganh10031996@gmail.com>
2024-12-17 12:29:29 +0700

    update

--- src/component/Report/Revenue.jsx
+++ src/component/Report/Revenue.jsx
@@ -8,9 +8,9 @@ dayjs.extend(quarterOfYear);
 import 'dayjs/locale/vi';
 dayjs.locale('vi');
 
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Chart as ChartJS, BarController, LineController, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 import { Chart } from 'react-chartjs-2';
-ChartJS.register(CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(BarController, LineController, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend);
 
 import { apiSearch, handleActionCallback } from "../Common/Utils";
 import PDFViewer from "../Common/PDFViewer";
